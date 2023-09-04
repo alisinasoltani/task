910b4af4d9ee6f6f13e3af7ccfb460a72cbf6b89
Alisina <alisinasoltani82@gmail.com>
2023-09-05 00:05:13 +0330

    Home Page UI
    Designed a Simple Home Page UI Containing a Navbar, Titles and Shortcut Links.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,9 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
     <title>Document</title>
 </head>
-<body>
-    
+<body class="bg-slate-900 w-full h-full">
+    <x-navbar />
+    <x-title />
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\InputController;
 use Illuminate\Support\Facades\Route;
 
 /*
